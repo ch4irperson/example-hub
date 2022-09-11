858a1738722380e432070664fa3905ef9a745ab1
ch4irperson <ceit.francisco@gmail.com>
2022-09-11 17:27:10 +0800

    Debug Log a_script

--- Group Project Sample/Assets/Scripts/a_script.cs
+++ Group Project Sample/Assets/Scripts/a_script.cs
@@ -7,7 +7,7 @@ public class a_script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("file testing");
     }
 
     // Update is called once per frame
